2acac316609c0121263c0c47d077793961e84499
woosung-21 <woosung.21@navercorp.com>
2023-07-18 15:02:11 +0900

    added controller for adding members

--- src/main/kotlin/woosung/learn/spring/controller/MemberController.kt
+++ src/main/kotlin/woosung/learn/spring/controller/MemberController.kt
@@ -1,10 +1,20 @@
 package woosung.learn.spring.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import woosung.learn.spring.service.MemberService
 
 @Controller
 class MemberController(
     private val memberService: MemberService
 ) {
+    @GetMapping("/members/new")
+    fun createForm(): String = "/members/createMemberForm"
+
+    @PostMapping("/members/new")
+    fun create(form: MemberForm): String {
+        memberService.join(form.name)
+        return "redirect:/"
+    }
 }
---
+++ src/main/kotlin/woosung/learn/spring/controller/MemberForm.kt
@@ -0,0 +1,5 @@
+package woosung.learn.spring.controller
+
+data class MemberForm(
+    val name: String
+)
