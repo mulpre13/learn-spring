e8fc34ad871d91a83f757ff930948f570e07954a
woosung-21 <woosung.21@navercorp.com>
2023-07-18 14:03:27 +0900

    modified duplicate user checks to throw exception, not function

--- src/main/kotlin/woosung/learn/spring/service/MemberService.kt
+++ src/main/kotlin/woosung/learn/spring/service/MemberService.kt
@@ -7,7 +7,7 @@ class MemberService(
     private val memberRepository: MemoryMemberRepository
 ) {
     fun join(name: String): Long {
-        memberRepository.findByName(name) ?: { throw IllegalStateException("User already exists") }
+        memberRepository.findByName(name)?.let { throw IllegalStateException("User already exists") }
         return memberRepository.save(name).id
     }
 
