cb7d674e371aa5aaae6055d349a371aa08b06b58
woosung-21 <woosung.21@navercorp.com>
2023-07-25 16:33:52 +0900

    added kotest-extensions-spring dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
 tasks.withType<KotlinCompile> {
