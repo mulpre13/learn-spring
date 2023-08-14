c67860089667d54e375ec14eb4996b215485fe7b
woosung-21 <woosung.21@navercorp.com>
2023-08-14 12:09:29 +0900

    removed unused imports

--- src/main/kotlin/woosung/learn/spring/SpringConfig.kt
+++ src/main/kotlin/woosung/learn/spring/SpringConfig.kt
@@ -1,10 +1,6 @@
 package woosung.learn.spring
 
-import jakarta.persistence.EntityManager
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import woosung.learn.spring.repository.JpaMemberRepository
-import woosung.learn.spring.repository.MemberRepository
 
 @Configuration
 class SpringConfig
